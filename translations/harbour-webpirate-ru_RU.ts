86f1ec7ba1632a39004b2c24b22b727420c58edb
Dax <trogu.davide@gmail.com>
2014-12-24 19:18:56 +0100

    Adjusted Changelog

--- translations/harbour-webpirate-ru_RU.ts
+++ translations/harbour-webpirate-ru_RU.ts
@@ -1,4 +1,6 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="ru_RU" version="2.0">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
 <context>
     <name>ActionSidebar</name>
     <message>
