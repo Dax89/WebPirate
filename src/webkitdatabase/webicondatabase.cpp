Dax89/WebPirate

commit 59e200f97680abc96c7935708dd7a0e893116449

Check if Favicon is available

src/webkitdatabase/webicondatabase.cpp
@@ -2,33 +2,47 @@
 
 const QString WebIconDatabase::WEBKIT_DATABASE = ".QtWebKit";
 const QString WebIconDatabase::ICON_DATABASE = "WebpageIcons.db";
-const QString WebIconDatabase::FAVICON_PROVIDER = "favicons";
+const QString WebIconDatabase::PROVIDER_NAME = "favicons";
 
-WebIconDatabase::WebIconDatabase(): QQuickImageProvider(QQuickImageProvider::Image)
+QSqlDatabase WebIconDatabase::_db;
+int WebIconDatabase::_refcount = 0;
+
+WebIconDatabase::WebIconDatabase(QObject *parent): QObject(parent)
 {
-    this->_db = QSqlDatabase::addDatabase("QSQLITE");
-}
+    if(!this->_refcount)
+        this->_db = QSqlDatabase::addDatabase("QSQLITE");
 
-QImage WebIconDatabase::requestImage(const QString& id, QSize* size, const QSize&)
-{
-    QByteArray ba = this->queryIconPixmap(id);
-
-    if(ba.isEmpty())
-        return QImage();
-
-    QImage img = QImage::fromData(ba);
-
-    if(img.isNull())
-        return QImage();
-
-    *size = img.size();
-    return img;
+    this->_refcount++;
 }
 
 WebIconDatabase::~WebIconDatabase()
 {
-    if(this->_db.isOpen())
-        this->_db.close();
+    this->_refcount--;
+
+    if(!this->_refcount)
+    {
+        if(this->_db.isOpen())
+            this->_db.close();
+    }
+}
+
+bool WebIconDatabase::hasIcon(const QString &url)
+{
+    QString s = this->queryIconUrl(url);
+
+    if(s.isEmpty())
+        return false;
+
+    QByteArray b = this->queryIconPixmap(url);
+    return !b.isEmpty();
+}
+
+QString WebIconDatabase::provideIcon(const QString &url)
+{
+    if(this->hasIcon(url))
+        return QString("image://%1/%2").arg(WebIconDatabase::PROVIDER_NAME, url);
+
+    return "image://theme/icon-m-favorite-selected";
 }
 
 bool WebIconDatabase::open()
@@ -62,6 +76,26 @@ int WebIconDatabase::queryIconId(const QString &url)
         return -1;
 
     return q.value(0).toInt();
+}
+
+QString WebIconDatabase::queryIconUrl(const QString &url)
+{
+    int iconid = this->queryIconId(url);
+
+    if(iconid == -1)
+        return QString();
+
+    QSqlQuery q(this->_db);
+
+    if(!this->prepare(q, "SELECT url FROM IconInfo WHERE iconID = ?"))
+        return QString();
+
+    q.bindValue(0, iconid);
+
+    if(!this->execute(q) || !q.first())
+        return QString();
+
+    return q.value(0).toString();
 }
 
 QByteArray WebIconDatabase::queryIconPixmap(const QString &url)

src/webkitdatabase/webicondatabase.h
@@ -4,34 +4,39 @@
 #include <QObject>
 #include <QDir>
 #include <QVariant>
+#include <QUrl>
 #include <QStandardPaths>
-#include <QQuickImageProvider>
 #include <QtSql/QSqlDatabase>
 #include <QtSql/QSqlQuery>
 #include <QtSql/QSqlError>
 #include <QDebug>
 
-class WebIconDatabase : public QQuickImageProvider
+class WebIconDatabase : public QObject
 {
+    Q_OBJECT
+
     public:
-        explicit WebIconDatabase();
-        QImage requestImage(const QString& id, QSize* size, const QSize&);
+        explicit WebIconDatabase(QObject* parent = 0);
+        QString queryIconUrl(const QString& url);
+        QByteArray queryIconPixmap(const QString& url);
         ~WebIconDatabase();
+
+    public slots:
+        QString provideIcon(const QString &url);
 
     private:
         bool open();
-        int queryIconId(const QString& url);
-        QByteArray queryIconPixmap(const QString& url);
-
-    private:
         bool prepare(QSqlQuery &queryobj, const QString& query);
         bool execute(QSqlQuery &queryobj);
+        bool hasIcon(const QString& url);
+        int queryIconId(const QString& url);
 
     private:
-        QSqlDatabase _db;
+        static QSqlDatabase _db;
+        static int _refcount;
 
     public:
-        static const QString FAVICON_PROVIDER;
+        static const QString PROVIDER_NAME;
 
     private:
         static const QString WEBKIT_DATABASE;

harbour-webpirate.pro
@@ -19,7 +19,8 @@ SOURCES += src/harbour-webpirate.cpp \
     src/webviewdatabase.cpp \
     src/downloadmanager/downloadmanager.cpp \
     src/downloadmanager/downloaditem.cpp \
-    src/webkitdatabase/webicondatabase.cpp
+    src/webkitdatabase/webicondatabase.cpp \
+    src/faviconprovider.cpp
 
 OTHER_FILES += qml/harbour-webpirate.qml \
     qml/cover/CoverPage.qml \
@@ -86,5 +87,6 @@ HEADERS += \
     src/webviewdatabase.h \
     src/downloadmanager/downloadmanager.h \
     src/downloadmanager/downloaditem.h \
-    src/webkitdatabase/webicondatabase.h
+    src/webkitdatabase/webicondatabase.h \
+    src/faviconprovider.h
 

qml/components/BrowserWebView.qml
@@ -102,15 +102,6 @@ SilicaWebView
                 Credentials.compile(Database.instance(), mainwindow.settings, url.toString(), webview);
                 History.store(url.toString(), title);
             }
-
-            /* FIXME: Check Favicon
-            if(navigationbar.favorite)
-            {
-                var favorite = mainwindow.settings.favorites.get(idx);
-                mainwindow.settings.favorites.fetchIcon(url.toString(), favorite);
-                Favorites.setIcon(Database.instance(), mainwindow.settings.favorites, url.toString(), favorite.icon);
-            }
-            */
         }
     }
 

qml/components/FavoritesView.qml
@@ -51,12 +51,7 @@ SilicaListView
                 height: Theme.iconSizeSmall
                 fillMode: Image.PreserveAspectFit
                 anchors.verticalCenter: parent.verticalCenter
-                source: "image://favicons/" + url
-
-                onStatusChanged: {
-                    if(status === Image.Error)
-                        source = "image://theme/icon-m-favorite-selected";
-                }
+                source: mainwindow.settings.icondatabase.provideIcon(url)
             }
 
             Label {

qml/components/NavigationBar.qml
@@ -2,6 +2,7 @@ import QtQuick 2.0
 import Sailfish.Silica 1.0
 import "../js/Database.js" as Database
 import "../js/Favorites.js" as Favorites
+import "../js/UrlHelper.js" as UrlHelper
 
 Rectangle
 {
@@ -70,8 +71,8 @@ Rectangle
 
     IconButton
     {
-        id: btnbookmark
-        width: Theme.iconSizeMedium
+        id: btnfavorite
+        width: visible ? Theme.iconSizeMedium : 0
         height: Theme.iconSizeMedium
         icon.source: (favorite ? "image://theme/icon-m-favorite-selected" : "image://theme/icon-m-favorite")
         anchors.verticalCenter: navigationbar.verticalCenter
@@ -92,10 +93,13 @@ Rectangle
     SearchBar
     {
         id: searchbar
-        anchors.left: btnbookmark.right
+        anchors.left: btnfavorite.right
         anchors.right: btnrefresh.left
-
         onReturnPressed: navigationbar.searchRequested(searchquery);
+
+        onUrlChanged: {
+            btnfavorite.visible = !UrlHelper.isSpecialUrl(searchbar.url);
+        }
     }
 
     IconButton

qml/js/History.js
@@ -1,6 +1,7 @@
 .pragma library
 
 .import QtQuick.LocalStorage 2.0 as Storage
+.import "UrlHelper.js" as UrlHelper
 
 function instance()
 {
@@ -39,6 +40,19 @@ function store(url, title)
     instance().transaction(function(tx) {
         tx.executeSql("INSERT OR REPLACE INTO History(url, title, lastvisit) VALUES(?, ?, DATETIME('now'))", [url, title]);
     });
+}
+
+function domainVisited(url)
+{
+    var visited = false;
+    var urlquery = "%" + UrlHelper.domainName(url) + "%";
+
+    instance().transaction(function(tx) {
+        var res = tx.executeSql("SELECT * FROM History WHERE url LIKE ? LIMIT 1", [urlquery]);
+        visited = res.rows.length > 0;
+    });
+
+    return visited;
 }
 
 function remove(url)

qml/models/Settings.qml
@@ -10,6 +10,7 @@ QtObject
     property SearchEngineModel searchengines: SearchEngineModel { }
     property ListModel useragents: ListModel { }
     property DownloadManager downloadmanager: DownloadManager { }
+    property WebIconDatabase icondatabase: WebIconDatabase { }
 
     property int searchengine;  /* Search Engine Index */
     property int useragent;     /* User Agent Index */

commit 80b8d290f392888d6a3581de264747fb9aba06aa

REGRESSION FIX: Removed Segmentation Fault on exit (bug introduced in 59e200f97680abc96c7935708dd7a0e893116449)

src/webkitdatabase/webicondatabase.cpp
@@ -1,16 +1,16 @@
 #include "webicondatabase.h"
 
+const QString WebIconDatabase::CONNECTION_NAME = "__wp__WebpageIcons";
 const QString WebIconDatabase::WEBKIT_DATABASE = ".QtWebKit";
 const QString WebIconDatabase::ICON_DATABASE = "WebpageIcons.db";
 const QString WebIconDatabase::PROVIDER_NAME = "favicons";
 
-QSqlDatabase WebIconDatabase::_db;
 int WebIconDatabase::_refcount = 0;
 
 WebIconDatabase::WebIconDatabase(QObject *parent): QObject(parent)
 {
     if(!this->_refcount)
-        WebIconDatabase::_db = QSqlDatabase::addDatabase("QSQLITE");
+        QSqlDatabase::addDatabase("QSQLITE", WebIconDatabase::CONNECTION_NAME);
 
     this->_refcount++;
 }
@@ -21,8 +21,13 @@ WebIconDatabase::~WebIconDatabase()
 
     if(!WebIconDatabase::_refcount)
     {
-        if(WebIconDatabase::_db.isOpen())
-            WebIconDatabase::_db.close();
+        QSqlDatabase db = QSqlDatabase::database(WebIconDatabase::CONNECTION_NAME, false);
+
+        if(db.isOpen())
+            db.close();
+
+        db = QSqlDatabase(); // Reset Database Reference
+        QSqlDatabase::removeDatabase(WebIconDatabase::CONNECTION_NAME);
     }
 }
 
@@ -47,7 +52,9 @@ QString WebIconDatabase::provideIcon(const QString &url)
 
 bool WebIconDatabase::open()
 {
-    if(this->_db.isOpen())
+    QSqlDatabase db = QSqlDatabase::database(WebIconDatabase::CONNECTION_NAME, false);
+
+    if(db.isOpen())
         return true;
 
     QDir dbpath = QDir(QStandardPaths::writableLocation(QStandardPaths::DataLocation));
@@ -55,8 +62,8 @@ bool WebIconDatabase::open()
     if(!dbpath.cd(WebIconDatabase::WEBKIT_DATABASE))
         return false;
 
-    this->_db.setDatabaseName(dbpath.absoluteFilePath(WebIconDatabase::ICON_DATABASE));
-    return this->_db.open();
+    db.setDatabaseName(dbpath.absoluteFilePath(WebIconDatabase::ICON_DATABASE));
+    return db.open();
 }
 
 int WebIconDatabase::queryIconId(const QString &url)
@@ -64,7 +71,8 @@ int WebIconDatabase::queryIconId(const QString &url)
     if(!this->open())
         return -1;
 
-    QSqlQuery q(this->_db);
+    QSqlDatabase db = QSqlDatabase::database(WebIconDatabase::CONNECTION_NAME, false);
+    QSqlQuery q(db);
     QString host = QUrl(url).host();
 
     if(host.isEmpty())
@@ -88,7 +96,8 @@ QString WebIconDatabase::queryIconUrl(const QString &url)
     if(iconid == -1)
         return QString();
 
-    QSqlQuery q(this->_db);
+    QSqlDatabase db = QSqlDatabase::database(WebIconDatabase::CONNECTION_NAME, false);
+    QSqlQuery q(db);
 
     if(!this->prepare(q, "SELECT url FROM IconInfo WHERE iconID = ?"))
         return QString();
@@ -108,7 +117,8 @@ QByteArray WebIconDatabase::queryIconPixmap(const QString &url)
     if(iconid == -1)
         return QByteArray();
 
-    QSqlQuery q(this->_db);
+    QSqlDatabase db = QSqlDatabase::database(WebIconDatabase::CONNECTION_NAME, false);
+    QSqlQuery q(db);
 
     if(!this->prepare(q, "SELECT data FROM IconData WHERE iconID = ?"))
         return QByteArray();

src/webkitdatabase/webicondatabase.h
@@ -32,13 +32,13 @@ class WebIconDatabase : public QObject
         int queryIconId(const QString& url);
 
     private:
-        static QSqlDatabase _db;
         static int _refcount;
 
     public:
         static const QString PROVIDER_NAME;
 
     private:
+        static const QString CONNECTION_NAME;
         static const QString WEBKIT_DATABASE;
         static const QString ICON_DATABASE;
 };