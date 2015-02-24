8019a092c4d622c429904eec5f0e684455411e84
Dax <trogu.davide@gmail.com>
2015-02-24 16:58:22 +0100

    Pixel Ratio Hack: Create a Viewport node if it doesn't exists, set minimum-scale value too

--- qml/js/helpers/ForcePixelRatio.js
+++ qml/js/helpers/ForcePixelRatio.js
@@ -9,17 +9,26 @@ var __wp_forcepixelratio__ = {
         if(document.location.hostname === "www.tagesschau.de") // Horrible Hack: Do not trigger DevicePixelRatio on this site
             return;
 
-        var viewport = document.querySelector("meta[name=viewport]");
+        var viewport = document.querySelector("meta[name='viewport']");
 
         if(!viewport)
-            return;
+        {
+            var heads = document.getElementsByTagName("head");
+
+            if((heads.length <= 0) || !heads[0])
+                return;
+
+            viewport = document.createElement("meta");
+            viewport.name = "viewport";
+            heads[0].appendChild(viewport);
+        }
 
         if(screen.width <= 540) /* Jolla devicePixelRatio: 1.5 */
-            viewport.setAttribute('content', 'width=device-width/1.5, initial-scale='+(1.5));
+            viewport.content = "width=device-width/1.5, minimum-scale=1.5, initial-scale=1.5";
         else if(screen.width > 540 && screen.width <= 768) /* Nexus 4 devicePixelRatio: 2.0 */
-            viewport.setAttribute('content', 'width=device-width/2.0, initial-scale='+(2.0));
+            viewport.content = "width=device-width/2.0, minimum-scale=2.0, initial-scale=2.0";
         else if (screen.width > 768) /* Nexus 5 devicePixelRatio: 3.0 */
-            viewport.setAttribute('content', 'width=device-width/3.0, initial-scale='+(3.0));
+            viewport.content = "width=device-width/3.0, minimum-scale=3.0, initial-scale=3.0";
     }
 }
 
