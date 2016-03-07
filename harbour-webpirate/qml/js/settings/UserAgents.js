b1aa4225b6902f1564b5a0297c3dd5924d592ecf
Dax89 <trogu.davide@gmail.com>
2016-03-07 17:06:31 +0100

    Added GoogleBot useragent (fixes #67)

--- harbour-webpirate/qml/js/settings/UserAgents.js
+++ harbour-webpirate/qml/js/settings/UserAgents.js
@@ -1,9 +1,10 @@
 .pragma library
 
-var defaultuseragents = [ {"type": "Mobile",  "value": "Mozilla/5.0 (U; Linux; Maemo; Jolla; Sailfish; like Android 4.3) AppleWebKit/538.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/538.1"},
-                          {"type": "Desktop", "value": "Mozilla/5.0 (X11; U; Linux i686; rv:25.0) Gecko/20100101 Firefox/25.0" },
-                          {"type": "Android", "value": "Mozilla/5.0 (Linux; Android 4.4; Nexus 4 Build/KRT16H) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/30.0.0.0 Mobile Safari/537.36" },
-                          {"type": "iPhone",  "value": "Mozilla/5.0 (iPhone; CPU iPhone OS 5_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9B179 Safari/7534.48.3" } ];
+var defaultuseragents = [ {"type": "Mobile",     "value": "Mozilla/5.0 (U; Linux; Maemo; Jolla; Sailfish; like Android 4.3) AppleWebKit/538.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/538.1"},
+                          {"type": "Desktop",    "value": "Mozilla/5.0 (X11; U; Linux i686; rv:25.0) Gecko/20100101 Firefox/25.0" },
+                          {"type": "Android",    "value": "Mozilla/5.0 (Linux; Android 4.4; Nexus 4 Build/KRT16H) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/30.0.0.0 Mobile Safari/537.36" },
+                          {"type": "iPhone",     "value": "Mozilla/5.0 (iPhone; CPU iPhone OS 5_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9B179 Safari/7534.48.3" },
+                          {"type": "GoogleBot",  "value": "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)" } ];
 
 function load(tx)
 {
